334361309bf37bb1b24de1f19c49752296795590
turtleDev <saravjeetamansingh@gmail.com>
2017-12-24 13:53:14 +0530

    add a stub server implementation

--- cmd/refract-api-server/main.go
+++ cmd/refract-api-server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/turtledev/refract-api/web"
 )
 
 func main() {
-	server := web.NewServer()
-	server.Start()
+	server := web.NewServer(":8080")
+	log.Println("starting api server on port 8080")
+	log.Fatalln(server.Start())
 }
--- web/server.go
+++ web/server.go
@@ -1,9 +1,23 @@
 package web
 
+import (
+	"net/http"
+)
+
+// Server Represents a wrapper around a net/http server
 type Server struct {
-	// todo
+	srv *http.Server
 }
 
-func NewServer() *Server {
-	return new(Server)
+// Start the server
+func (server *Server) Start() error {
+	return server.srv.ListenAndServe()
+}
+
+// NewServer creates a new Server instance
+func NewServer(addr string) *Server {
+	server := new(Server)
+	server.srv = new(http.Server)
+	server.srv.Addr = addr
+	return server
 }
